62adb8eea06c84ef8aec0e02e24ec6b046c09dcd
Cosmin Dinu <cdcdd15@gmail.com>
2021-05-16 14:48:41 +0300

    Attempt to integrate ELK containers (failed).

--- src/main/java/com/cosmin/SpringBootMicroApplication.java
+++ src/main/java/com/cosmin/SpringBootMicroApplication.java
@@ -4,11 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+import com.cosmin.logs.StaticLogs;
+
+import lombok.extern.slf4j.Slf4j;
+
 @SpringBootApplication
 @EnableDiscoveryClient
+@Slf4j
 public class SpringBootMicroApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootMicroApplication.class, args);
+		String message = "is started";
+		StaticLogs.staticLogs(log, message);
 	}
 }
--- src/main/java/com/cosmin/beans/MicroTwoController.java
+++ src/main/java/com/cosmin/beans/MicroTwoController.java
@@ -5,12 +5,18 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cosmin.logs.StaticLogs;
+
+import lombok.extern.slf4j.Slf4j;
+
 @RestController
-public class EmployeeController {
+@Slf4j
+public class MicroTwoController {
 
 	@GetMapping("/hello-world")
 	public ResponseEntity<String> hello() {
-		System.out.println("In 07-micro-2");
+//		System.out.println("In 07-micro-2");
+		StaticLogs.staticLogs(log, "from REST endpoint");
 		return new ResponseEntity<String>("Hello world updated 2 (server)", HttpStatus.OK);
 	}
 	
