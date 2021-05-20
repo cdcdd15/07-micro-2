e31f76273b1079090ec02c3ecb1f51c0e8869473
Cosmin Dinu <cdcdd15@gmail.com>
2021-05-20 18:52:25 +0300

    Attempt to integrate ELK containers succeded.

--- src/main/java/com/cosmin/logs/StaticLogs.java
+++ src/main/java/com/cosmin/logs/StaticLogs.java
@@ -2,8 +2,8 @@ package com.cosmin.logs;
 
 public class StaticLogs {
 	public static void staticLogs(org.slf4j.Logger log, String message) {
-		log.info("<info>The application 07-micro-1 - {}.", message);
-		log.error("<error>The application 07-micro-1 - {}.", message);
-		log.debug("<debug>The application 07-micro-1 - {}.", message);
+		log.info("<info>The application 07-micro-2 - {}.", message);
+		log.error("<error>The application 07-micro-2 - {}.", message);
+		log.debug("<debug>The application 07-micro-2 - {}.", message);
 	}
 }
